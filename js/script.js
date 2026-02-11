9317630db22585f61e1a98a882a8a77df9e16664
tortiman <josejavierbedmar@gmail.com>
2026-02-11 21:10:53 +0100

    final commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const nav = document.querySelector('.header__nav--mobile');
+    const btn = document.querySelector('.header__hamburger');
+    const btnClose=document.querySelector('.header__close');
+
+    btn.addEventListener('click', () => {
+    const current = getComputedStyle(nav).display;
+    nav.style.display = current === 'none' ? 'block' : 'none';
+    });
+
+    btnClose.addEventListener('click', () => {
+        nav.style.display = 'none';
+    
+    });
+});
